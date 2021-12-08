cd8098acf4741a844fb259478d3c566286b77029
mominnaveed <mominnaveed220@gmail.com>
2021-12-08 13:42:09 +0500

    Drum Kit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-// for (var i = 0; i < numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-//     document.querySelectorAll(".drum")[i].addEventListener("Click", function () {
-//         alert("I got Clicked!");
-//     });
-// }
-var numberOfButtons = document.querySelectorAll(".button").length;
-
-for (var j = 0; j < numberOfButtons; j++) {
-
-    document.querySelectorAll(".button")[j]
-        .addEventListener("click", function () {
-            var buttonStyle = this.innerHTML;
-            sound(buttonStyle);
-            animation(buttonStyle);
-        });
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got Clicked!");
+    });
 }
+
+// var numberOfButtons = document.querySelectorAll("button").length;
+
+// for (var j = 0; j < numberOfButtons; j++) {
+
+//     document.querySelectorAll("button")[j]
+//         .addEventListener("click", function () {
+//             // var buttonStyle = this.innerHTML;
+//             // sound(buttonStyle);
+//             // animation(buttonStyle);
+//             alert("Hi")
+//         });
+// }
